0df4ad6d93ed85b2d32a6bc7d0fbd50194e681df
Marion Terray <mterray@gmail.com>
2023-04-15 12:46:04 +0200

    adding render

--- vanilla-build/js/app.js
+++ vanilla-build/js/app.js
@@ -21,33 +21,33 @@ function init() {
     const view = new View();
     const store = new Store('live-t3-storage-key', players);
 
-    function initView() {
-      view.closeAll();
-      view.clearMoves();
-      view.setTurnIndicator(store.game.currentPlayer);
-      view.updateScoreboard(
-        store.stats.playerWithStats[0].wins,
-        store.stats.playerWithStats[1].wins,
-        store.stats.ties,
-        );
-    view.initializeMoves(store.game.moves);
-    }
+   // function initView() {
+   //   view.closeAll();
+   //   view.clearMoves();
+   //   view.setTurnIndicator(store.game.currentPlayer);
+   //   view.updateScoreboard(
+   //     store.stats.playerWithStats[0].wins,
+   //     store.stats.playerWithStats[1].wins,
+   //     store.stats.ties,
+   //     );
+   // view.initializeMoves(store.game.moves);
+   // }
 
     window.addEventListener('storage', () => {
       console.log('State change from another tab');
-      initView()
+      view.render(store.game, store.stats);
     });
 
-    initView();
+    view.render(store.game, store.stats);
 
     view.bindGameResetEvent((event) => {
         store.reset();
-        initView();
+        view.render(store.game, store.stats);
     });
 
     view.bindNewRoundEvent((event) => {
         store.newRound();
-        initView();
+        view.render(store.game, store.stats);
     });
 
     view.bindPlayerMoveEvent((square) => {
@@ -61,22 +61,24 @@ function init() {
         }
 
         //place an icon of the current player 
-        view.handlePlayerMove(square, store.game.currentPlayer);
+        //view.handlePlayerMove(square, store.game.currentPlayer);
 
         //advance to the next state by pushing a move to the moves array
         store.playerMove(+square.id);
 
-        if (store.game.status.isComplete) {
-          view.opendModal(store.game.status.winner 
-          ? `${store.game.status.winner.name} wins !`
-          : `Tie !`
-          );
+        view.render(store.game, store.stats);
 
-          return;
-        }
-
+       // if (store.game.status.isComplete) {
+       //   view.opendModal(store.game.status.winner 
+       //   ? `${store.game.status.winner.name} wins !`
+       //   : `Tie !`
+       //   );
+//
+       //   return;
+       // }
+//
         //set the next player's turn indicator
-        view.setTurnIndicator(store.game.currentPlayer);
+        //view.setTurnIndicator(store.game.currentPlayer);
     });
 
 }
